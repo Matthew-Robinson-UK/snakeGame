18bfe7babc8e9b2abd99a0cdf1e98edb3de5d513
Matthew Robinson <robinsonm18@cardiff.ac.uk>
2022-03-01 11:39:22 +0000

    added methods

--- gamePanel.java
+++ gamePanel.java
@@ -1,13 +1,40 @@
-import javax.swing.JPanel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
 
 public class gamePanel extends JPanel implements ActionListener {
 
     gamePanel(){
-        
-    }
 
+    }
+    public void startGame(){
+
+    }
+    public void paintComponent(Graphics g){
+
+    }
+    public void draw(Graphics g){
+
+    } 
+    public void move() {
+
+    }
+    public void checkApple(){
+
+    }
+    public void checkCollitions(){
+
+    }
+    public void gameOver(Graphics g) {
+
+    }
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
