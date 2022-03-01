e7ff16f7aa7f7f30f18947edde16888aeec4b9a2
Matthew Robinson <robinsonm18@cardiff.ac.uk>
2022-03-01 11:47:54 +0000

    update start game method

--- gamePanel.java
+++ gamePanel.java
@@ -5,11 +5,36 @@ import java.util.Random;
 
 public class gamePanel extends JPanel implements ActionListener {
 
-    gamePanel(){
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = ((SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE);
+    static final int DELAY = 75;
 
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten = 0;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+
+    gamePanel(){
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
     public void startGame(){
-
+        newApple();
+        running = true;
+        timer = new Timer(DELAY,this);
+        timer.start();
     }
     public void paintComponent(Graphics g){
 
@@ -17,6 +42,9 @@ public class gamePanel extends JPanel implements ActionListener {
     public void draw(Graphics g){
 
     } 
+    public void newApple(){
+
+    }
     public void move() {
 
     }
